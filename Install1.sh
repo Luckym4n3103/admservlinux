620df26a5da957349be11b608aba1f6d711be459
Luckym4n3103 <83238519+Luckym4n3103@users.noreply.github.com>
2021-09-25 12:29:44 -0300

    Update Install1.sh

--- Install1.sh
+++ Install1.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash 
+sudo apt update -y &&
+echo "update foi realizado com sucesso " &&
+sudo apt upgrade -y && 
+echo "upgrade foi realizado com sucesso" &&
+sudo apt install apache2 bind9 dnsutils openssh-server openssh-client whois net-tools systemctl && 
+echo "instalação do "
